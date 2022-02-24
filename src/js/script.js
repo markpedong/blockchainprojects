08ddf9ec4f101c8ddee44b1a5595ccdc9b6b3022
markpedong <mark.pedong01@gmail.com>
2022-02-24 19:32:49 +0800

    Finished the design for the result

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,10 @@
 import * as animation from "./animation.js";
+
+// const button = document.querySelectorAll("#card__header");
+// const btnNetwork = document.querySelector("#btn__network");
+
+// button.forEach((el) => {
+//   el.addEventListener("click", function () {
+//     console.log(btnNetwork);
+//   });
+// });
