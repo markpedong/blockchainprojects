be6ca66f0a708999b64e956143a2c60f841269be
markpedong <mark.pedong01@gmail.com>
2022-02-27 19:08:34 +0800

    Will continue tomorrow.

---
+++ src/js/collapse.js
@@ -0,0 +1,18 @@
+const test = document.querySelectorAll("#coin__container");
+const button = document.querySelector("#btn__collapse");
+const viewMore = document.querySelector("#moreinfo__button");
+const collapseList = document.querySelector("#collapseContainer");
+
+viewMore.addEventListener("click", function () {
+  test.forEach((el) => {
+    el.style.display = "none";
+  });
+});
+
+button.addEventListener("click", function () {
+  test.forEach((el) => {
+    el.style.display = "block";
+  });
+
+  collapseList.classList.remove("show");
+});
--- src/js/script.js
+++ src/js/script.js
@@ -1,2 +1,25 @@
 import * as animation from "./animation.js";
 import * as navbar from "./navbar.js";
+import * as collapse from "./collapse.js";
+import * as networkselector from "./networkselector.js";
+
+export default class BlockChain {
+  constructor() {
+    this._networkSelector();
+  }
+
+  _networkSelector() {
+    const networks = document.querySelectorAll("#card__header");
+    const selectNetwork = document.querySelector("#select__network");
+
+    networks.forEach((el) =>
+      el.addEventListener("click", function (e) {
+        const network = e.target;
+
+        selectNetwork.innerHTML = network.innerHTML;
+      })
+    );
+  }
+}
+
+new BlockChain();
