e7f22129df1d097ee085e663bef3d4ee671146ad
markpedong <mark.pedong01@gmail.com>
2022-02-27 13:49:35 +0800

    links are working fine

---
+++ src/js/navbar.js
@@ -0,0 +1,32 @@
+// Navlinks
+const homeNav = document.querySelector(".home__nav");
+const projectNav = document.querySelector(".projects__nav");
+const contactNav = document.querySelector(".contact__nav");
+const homeFooter = document.querySelector(".home__footer");
+const projectsFooter = document.querySelector(".projects__footer");
+const contactFooter = document.querySelector(".contact__footer");
+
+// Sections
+const navPage = document.querySelector("#nav__container");
+const featuresPage = document.querySelector("#features__section");
+const footerPage = document.querySelector("#footer__section");
+
+homeNav.addEventListener("click", function () {
+  navPage.scrollIntoView({ behavior: "smooth" });
+});
+projectNav.addEventListener("click", function () {
+  featuresPage.scrollIntoView({ behavior: "smooth" });
+});
+contactNav.addEventListener("click", function () {
+  footerPage.scrollIntoView({ behavior: "smooth" });
+});
+
+homeFooter.addEventListener("click", function () {
+  navPage.scrollIntoView({ behavior: "smooth" });
+});
+projectsFooter.addEventListener("click", function () {
+  featuresPage.scrollIntoView({ behavior: "smooth" });
+});
+contactFooter.addEventListener("click", function () {
+  footerPage.scrollIntoView({ behavior: "smooth" });
+});
--- src/js/script.js
+++ src/js/script.js
@@ -1,10 +1,2 @@
 import * as animation from "./animation.js";
-
-// const button = document.querySelectorAll("#card__header");
-// const btnNetwork = document.querySelector("#btn__network");
-
-// button.forEach((el) => {
-//   el.addEventListener("click", function () {
-//     console.log(btnNetwork);
-//   });
-// });
+import * as navbar from "./navbar.js";
